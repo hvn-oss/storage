f267d319fc38ff1c7c97347158d20c310243488c
Daniel Fakunle <tosinfakunle@yahoo.com>
2026-08-05 20:52:09 -0500

    feat: setup shadcn

--- apps/docs/src/routes/__root.tsx
+++ apps/docs/src/routes/__root.tsx
@@ -3,6 +3,7 @@ import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 import { TanStackDevtools } from "@tanstack/react-devtools";
 
 import appCss from "../styles.css?url";
+import { ThemeProvider } from "#/components/theme-provider.tsx";
 
 export const Route = createRootRoute({
   head: () => ({
@@ -30,12 +31,14 @@ export const Route = createRootRoute({
 
 function RootDocument({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <HeadContent />
       </head>
       <body>
-        {children}
+        <ThemeProvider defaultTheme="system" storageKey="theme">
+          {children}
+        </ThemeProvider>
         <TanStackDevtools
           config={{
             position: "bottom-right",
--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,13 @@ export default defineConfig({
   },
   fmt: {
     proseWrap: "always",
+    ignorePatterns: ["routeTree.gen.ts"],
   },
   lint: {
     jsPlugins: [{ name: "vite-plus", specifier: "vite-plus/oxlint-plugin" }],
     rules: { "vite-plus/prefer-vite-plus-imports": "error" },
     options: { typeAware: true, typeCheck: true },
+    ignorePatterns: ["routeTree.gen.ts"],
   },
   run: {
     cache: true,
